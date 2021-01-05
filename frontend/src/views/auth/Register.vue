b9d05ada6e329983d3dcdb4188521b4860fc23b3
DESKTOP-IB5E4D7\David <david4obgg1@gmail.com>
2021-01-05 11:47:34 +0200

    First commit

---
+++ frontend/src/views/auth/Register.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="container mx-auto px-4 h-full">
+    <div class="flex content-center items-center justify-center h-full">
+      <div class="w-full lg:w-6/12 px-4">
+        <div
+          class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0"
+        >
+          <div class="rounded-t mb-0 px-6 py-6">
+            <div class="text-center mb-3">
+              <h3 class="text-gray-600 text-xl font-bold">
+                Sign up
+              </h3>
+            </div>
+          </div>
+          <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+            <FormulateForm v-model="values" @submit="submitted">
+              <FormulateInput
+                type="text"
+                name="First name"
+                label="Enter your First name"
+                validation="^required|min:5|max:50"
+                outer-class="mb-4"
+                input-class="border border-gray-400 rounded px-3 py-2 leading-none focus:border-green-500 outline-none border-box w-full mb-1"
+                label-class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                help-class="text-xs mb-1 text-gray-600"
+                error-class="text-red-700 bg-red-700 text-xs mb-1"
+              />
+              <FormulateInput
+                type="text"
+                name="Middle name"
+                label="Enter your Middle name"
+                validation="^required|min:5|max:50"
+                outer-class="mb-4"
+                input-class="border border-gray-400 rounded px-3 py-2 leading-none focus:border-green-500 outline-none border-box w-full mb-1"
+                label-class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                help-class="text-xs mb-1 text-gray-600"
+                error-class="text-red-700 text-xs mb-1"
+              />
+              <FormulateInput
+                type="text"
+                name="Last name"
+                label="Enter your Last name"
+                validation="^required|min:5|max:50"
+                outer-class="mb-4"
+                input-class="border border-gray-400 rounded px-3 py-2 leading-none focus:border-green-500 outline-none border-box w-full mb-1"
+                label-class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                help-class="text-xs mb-1 text-gray-600"
+                error-class="text-red-700 text-xs mb-1"
+              />
+              <FormulateInput
+                type="email"
+                name="email"
+                label="Enter your Email address"
+                validation="email"
+                outer-class="mb-4"
+                input-class="border border-gray-400 rounded px-3 py-2 leading-none focus:border-green-500 outline-none border-box w-full mb-1"
+                label-class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                help-class="text-xs mb-1 text-gray-600"
+                error-class="text-red-700 text-xs mb-1"
+              />
+              <FormulateInput
+                type="password"
+                name="password"
+                label="Password"
+                validation="^required|min:5,length|matches:/[0-9]/"
+                :validation-messages="{
+                  matches: 'Password must contain at least 1 number.',
+                }"
+                outer-class="mb-4"
+                input-class="border border-gray-400 rounded px-3 py-2 leading-none focus:border-green-500 outline-none border-box w-full mb-1"
+                label-class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                help-class="text-xs mb-1 text-gray-600"
+                error-class="text-red-700 text-xs mb-1"
+              />
+              <FormulateInput
+                type="password"
+                name="password_confirm"
+                label="Confirm password"
+                validation="^required|confirm"
+                validation-name="Password confirmation"
+                outer-class="mb-4"
+                input-class="border border-gray-400 rounded px-3 py-2 leading-none focus:border-green-500 outline-none border-box w-full mb-1"
+                label-class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                help-class="text-xs mb-1 text-gray-600"
+                error-class="text-red-700 text-xs mb-1"
+              />
+              <FormulateInput
+                input-class="px-4 py-2 rounded bg-blue-500 text-white hover:bg-blue-700"
+                type="submit"
+              />
+            </FormulateForm>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
